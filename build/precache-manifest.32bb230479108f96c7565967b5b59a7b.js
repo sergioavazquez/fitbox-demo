fbd7f1f878043ece4ff30ec7e8834f62e47774e7
Sergio Vazquez <sergio.vazquez@rga.com>
2020-01-09 16:22:25 -0300

    working on ghpages deployment

---
+++ build/precache-manifest.32bb230479108f96c7565967b5b59a7b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13482702e0029b42c022f8b26078fc58",
+    "url": "/fitbox-demo/index.html"
+  },
+  {
+    "revision": "de45b8580d5409345dbf",
+    "url": "/fitbox-demo/static/css/2.bf79a5e1.chunk.css"
+  },
+  {
+    "revision": "4785399d73d592cfe001",
+    "url": "/fitbox-demo/static/css/main.f22b4d5a.chunk.css"
+  },
+  {
+    "revision": "de45b8580d5409345dbf",
+    "url": "/fitbox-demo/static/js/2.118eb4c7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/fitbox-demo/static/js/2.118eb4c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4785399d73d592cfe001",
+    "url": "/fitbox-demo/static/js/main.c1bc78d7.chunk.js"
+  },
+  {
+    "revision": "7f7f37b5e96f4ecc81cb",
+    "url": "/fitbox-demo/static/js/runtime-main.7b2756b8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fitbox-demo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "685431ef1853149a722b197169db4233",
+    "url": "/fitbox-demo/static/media/morty-sprite.685431ef.png"
+  },
+  {
+    "revision": "30c5226784820690901a33d4ce3b9466",
+    "url": "/fitbox-demo/static/media/rick-sprite.30c52267.png"
+  }
+]);
